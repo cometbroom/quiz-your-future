8a42965b46900001a4f6051233a28dc4bbe3ff78
cometbroom <alawi.wow@gmail.com>
2022-03-14 17:51:59 +0100

    Create gsap environment

--- src/components/animations.js
+++ src/components/animations.js
@@ -1,23 +1,33 @@
+import { USER_INTERFACE_ID } from '../constants.js';
+import { gsap } from '../../_snowpack/pkg/gsap.js';
+
 let intervalPlaceholder;
 const opacityAdder = 0.02;
 
 //Set opacity of element to 0 and animate it to 1
 export const opacityAnimation = (el) => {
-	el.style.opacity = "0";
-	
-	intervalPlaceholder = setInterval(intervalMethod(el), 4);
-}
+  el.style.opacity = '0';
+
+  intervalPlaceholder = setInterval(intervalMethod(el), 4);
+};
 
 const intervalMethod = (el) => {
-	return () => {
-		let opacity = parseFloat(el.style.opacity);
+  return () => {
+    let opacity = parseFloat(el.style.opacity);
 
-		opacity += opacityAdder;
-		el.style.opacity = opacity;
-		if (opacity >= 1) {
-			clearInterval(intervalPlaceholder);
-			return;
-		}
-	}
+    opacity += opacityAdder;
+    el.style.opacity = opacity;
+    if (opacity >= 1) {
+      clearInterval(intervalPlaceholder);
+      return;
+    }
+  };
+};
+
+export function sideAnimation(from, to, delay = 0) {
+  gsap.fromTo(
+    '#user-interface',
+    { left: from },
+    { left: to, duration: 0.4, delay: delay }
+  );
 }
-
