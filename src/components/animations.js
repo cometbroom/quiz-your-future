38a3f83c66ab933960711344464dd8841f9f96d0
cometbroom <alawi.wow@gmail.com>
2022-03-14 15:47:59 +0100

    Imported gsap

--- src/components/animations.js
+++ src/components/animations.js
@@ -1,12 +1,11 @@
 let intervalPlaceholder;
-let opacityAdder = 0.02;
+const opacityAdder = 0.02;
 
 //Set opacity of element to 0 and animate it to 1
 export const opacityAnimation = (el) => {
 	el.style.opacity = "0";
 	
 	intervalPlaceholder = setInterval(intervalMethod(el), 4);
-
 }
 
 const intervalMethod = (el) => {
